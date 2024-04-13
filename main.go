7333f2dbf673e6569c8e91dd6eaaf90c0bf5e9c0
i9 <oluwarinolasam@gmail.com>
2024-04-13 01:57:59 +0100

    local storage location

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "i9rfs/client/cmd"
+import (
+	"i9pkgs/i9services"
+	"i9rfs/client/cmd"
+	"log"
+)
 
 func main() {
+	if err := i9services.InitLocalStorage("i9rfs", "localStorage.json"); err != nil {
+		log.Fatal(err)
+	}
+
+	i9services.LocalStorage.SetItem("bar", "foo")
+
 	cmd.Execute()
 }
