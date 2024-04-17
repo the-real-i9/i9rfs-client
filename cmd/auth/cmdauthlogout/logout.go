46655b3137267055b719c80a52ef1b0d8162dc23
i9 <oluwarinolasam@gmail.com>
2024-04-17 16:36:01 +0100

    logout message

--- cmd/auth/cmdauthlogout/logout.go
+++ cmd/auth/cmdauthlogout/logout.go
@@ -1,7 +1,12 @@
 package cmdauthlogout
 
-import "i9pkgs/i9services"
+import (
+	"fmt"
+	"i9pkgs/i9services"
+)
 
 func Execute() {
 	i9services.LocalStorage.DeleteItem("auth_jwt")
+
+	fmt.Println("You've been logged out!")
 }
